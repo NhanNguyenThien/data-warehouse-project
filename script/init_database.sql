7d3ac26963438ac8f26e22fe4b23a26671cef263
NhanNguyenThien <129637316+NhanNguyenThien@users.noreply.github.com>
2026-07-18 21:26:52 +0700

    Create init_database.sql

---
+++ script/init_database.sql
@@ -0,0 +1,17 @@
+/*
+===============================================================================
+Script: Khởi tạo Schema cho Data Warehouse
+===============================================================================
+Mục đích:
+    Tạo ba schema bronze, silver, gold trong Fabric Warehouse.
+===============================================================================
+*/
+
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
